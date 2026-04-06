56543c6b6f71fce0d9ca5fa1627309059ab6b8c3
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-05 21:58:17 -0600

    scripts

--- modules/features/shell.nix
+++ modules/features/shell.nix
@@ -1,3 +1,13 @@
+let
+  externalRepo = pkgs.fetchFromGitHub {
+    owner = "tavocg";
+    repo = "dotfiles";
+    rev = "a2255e0";
+    sha256 = "0000000000000000000000000000000000000000000000000000"; # placeholder
+  };
+
+  scriptsDir = "${externalRepo}/scripts";
+in
 { ... }: {
   flake.nixosModules.shell = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
@@ -5,9 +15,10 @@
       neovim
       git
       lazygit
-      nodejs
       gcc
       yazi
     ];
+
+    environment.sessionVariables.PATH = "${scriptsDir}:${builtins.getEnv "PATH"}";
   };
 }
