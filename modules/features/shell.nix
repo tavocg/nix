dba9abb955fa12ac4c0c1ea2a6af680f13b31bbd
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-05 21:58:52 -0600

    no

--- modules/features/shell.nix
+++ modules/features/shell.nix
@@ -1,13 +1,3 @@
-let
-  externalRepo = pkgs.fetchFromGitHub {
-    owner = "tavocg";
-    repo = "dotfiles";
-    rev = "a2255e0";
-    sha256 = "0000000000000000000000000000000000000000000000000000"; # placeholder
-  };
-
-  scriptsDir = "${externalRepo}/scripts";
-in
 { ... }: {
   flake.nixosModules.shell = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
@@ -18,7 +8,5 @@ in
       gcc
       yazi
     ];
-
-    environment.sessionVariables.PATH = "${scriptsDir}:${builtins.getEnv "PATH"}";
   };
 }
