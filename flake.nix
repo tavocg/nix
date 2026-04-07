0c3e31a34e1f886f61d7eec1c166d77ab66a70e8
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-07 15:50:54 -0600

    dotfiles

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+    dotfiles.url = "github:tavocg/dotfiles";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
