8970f951fd4ec2e8a80431539f042f70e2331f61
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-07 14:18:37 -0600

    profile

--- modules/features/shell/default.nix
+++ modules/features/shell/default.nix
@@ -3,6 +3,14 @@
     imports = [
     ];
 
+    environment.shellInit = ''
+      for f in ~/.config/shell/profile.d/*.sh; do
+        if [ -r "$f" ]; then
+          . "$f"
+        fi
+      done
+    '';
+
     environment.systemPackages = with pkgs; [
       git
       lazygit
