ea65bb250c5ea32718daf77dc97e66412758e628
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-07 16:40:35 -0600

    not needed

--- modules/features/shell/default.nix
+++ modules/features/shell/default.nix
@@ -1,9 +1,5 @@
 { self, ... }: {
   flake.nixosModules.shell = { pkgs, ... }: {
-    imports = [
-      self.nixosModules.bash
-    ];
-
     users.defaultUserShell = self.packages.${pkgs.stdenv.hostPlatform.system}.bash;
     environment.shells = [
       "/run/current-system/sw/bin/bash"
