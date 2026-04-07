96825718573efd0b5350ec341e3cd6356cd95702
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-07 16:35:31 -0600

    clean

--- modules/features/shell/default.nix
+++ modules/features/shell/default.nix
@@ -1,14 +1,13 @@
 { self, ... }: {
-  flake.nixosModules.shell = { pkgs, ... }: let
-    bashPackage = self.packages.${pkgs.stdenv.hostPlatform.system}.bash;
-  in {
+  flake.nixosModules.shell = { pkgs, ... }: {
     imports = [
+      self.nixosModules.bash
     ];
 
-    users.defaultUserShell = bashPackage;
+    users.defaultUserShell = self.packages.${pkgs.stdenv.hostPlatform.system}.bash;
     environment.shells = [
       "/run/current-system/sw/bin/bash"
-      "${bashPackage}/bin/bash"
+      "${self.packages.${pkgs.stdenv.hostPlatform.system}.bash}/bin/bash"
     ];
   };
 }
