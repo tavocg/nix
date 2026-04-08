9f920fecc12b2ca7df11a04df40b03484657a796
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-08 00:20:34 -0600

    fix

--- modules/features/system/cr.nix
+++ modules/features/system/cr.nix
@@ -1,19 +1,12 @@
 { ... }: {
   flake.nixosModules.cr = { pkgs, ... }: {
-    time.timeZone = "America/Costa_Rica";
+    i18n.supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "es_CR.UTF-8/UTF-8"
+    ];
 
     i18n.defaultLocale = "en_US.UTF-8";
 
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "es_CR.UTF-8";
-      LC_IDENTIFICATION = "es_CR.UTF-8";
-      LC_MEASUREMENT = "es_CR.UTF-8";
-      LC_MONETARY = "es_CR.UTF-8";
-      LC_NAME = "es_CR.UTF-8";
-      LC_NUMERIC = "es_CR.UTF-8";
-      LC_PAPER = "es_CR.UTF-8";
-      LC_TELEPHONE = "es_CR.UTF-8";
-      LC_TIME = "es_CR.UTF-8";
-    };
+    time.timeZone = "America/Costa_Rica";
   };
 }
