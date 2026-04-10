ed634041a1618bb9a921a9524af4095dee932958
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-09 23:47:52 -0600

    sway wrapper dotfiles

--- modules/features/sway.nix
+++ modules/features/sway.nix
@@ -1,7 +1,41 @@
-{ inputs, ... }: {
+{ inputs, self, ... }: {
+  imports = [
+    inputs.wrapper-modules.flakeModules.wrappers
+  ];
+
+  flake.wrappers.sway = { config, pkgs, wlib, ... }: let
+    swayConfigPath = config.constructFiles.config.path;
+  in {
+    imports = [ wlib.modules.default ];
+
+    config = {
+      package = pkgs.sway;
+
+      flags."--config" = swayConfigPath;
+
+      constructFiles.config = {
+        relPath = "config";
+        content = builtins.readFile "${inputs.dotfiles}/sway/config";
+      };
+    };
+  };
+
+  perSystem = { config, ... }: {
+    wrappers = {
+      control_type = "build";
+      packages.sway = true;
+    };
+
+    apps.sway = {
+      type = "app";
+      program = "${config.packages.sway}/bin/sway";
+    };
+  };
+
   flake.nixosModules.sway = { pkgs, ... }: {
     programs.sway = {
       enable = true;
+      package = self.packages.${pkgs.stdenv.hostPlatform.system}.sway;
       wrapperFeatures.gtk = true;
 
       extraPackages = with pkgs; [
