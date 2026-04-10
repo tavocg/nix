e5706fb890e86dcb67c5ac0e5b7e78674a6add9c
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-10 11:02:01 -0600

    works

--- modules/hosts/laptop/configuration.nix
+++ modules/hosts/laptop/configuration.nix
@@ -9,8 +9,8 @@
       self.nixosModules.system
       self.nixosModules.iwd
       self.nixosModules.bt
-      self.nixosModules.i3Autologin
-      self.nixosModules.i3
+      self.nixosModules.swayAutologin
+      self.nixosModules.sway
     ];
 
     networking.hostName = "laptop";
