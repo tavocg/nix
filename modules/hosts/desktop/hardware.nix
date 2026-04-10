b045f8762993e6e4addeb54b4193c986d90ceb86
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-10 11:17:14 -0600

    fix

--- modules/hosts/desktop/hardware.nix
+++ modules/hosts/desktop/hardware.nix
@@ -1,5 +1,5 @@
 { self, inputs, ... }: {
-  flake.nixosModules.laptopHardware = { config, lib, pkgs, modulesPath, ... }: {
+  flake.nixosModules.desktopHardware = { config, lib, pkgs, modulesPath, ... }: {
 imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
