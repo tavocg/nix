364fc81dff28bb139e0000fd5b515705e5d22b6d
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-10 10:50:13 -0600

    nvidia

---
+++ modules/features/nvidia/proprietary.nix
@@ -0,0 +1,35 @@
+{ self, ... }: {
+  # Note: this would work in desktop but not in a laptop,
+  # for laptop create something like nvidiaProprietaryPrime
+  flake.nixosModules.nvidiaProprietary = { pkgs, ... }: {
+    hardware.graphics.enable = true;
+    services.xserver.videoDrivers = ["nvidia"]; # (Xorg and Wayland)
+
+    hardware.nvidia = {
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+      # of just the bare essentials.
+      powerManagement.enable = false;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of 
+      # supported GPUs is at: 
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+      # Only available from driver 515.43.04+
+      open = false;
+
+      nvidiaSettings = true; # Nvidia settings menu,
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+  };
+}
