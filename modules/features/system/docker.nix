3bc95b1bbfb3ff7f29b22404268922f8d3c42561
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-10 01:16:08 -0600

    docker

---
+++ modules/features/system/docker.nix
@@ -0,0 +1,9 @@
+{ ... }: {
+  flake.nixosModules.docker = { pkgs, ... }: {
+    virtualisation.docker.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      lazydocker
+    ];
+  };
+}
