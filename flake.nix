3893a75557a1cb0fb18feb3254540d1a7e418224
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-09 23:53:29 -0600

    do not duplicate

--- flake.nix
+++ flake.nix
@@ -1,20 +1,44 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
-    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
 
-    shtatus.url = "github:tavocg/shtatus";
-    anypinentry.url = "github:tavocg/anypinentry";
-    kanban.url = "github:tavocg/kanban";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    import-tree.url = "github:vic/import-tree";
+
+    wrapper-modules = {
+      url = "github:BirdeeHub/nix-wrapper-modules";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    shtatus = {
+      url = "github:tavocg/shtatus";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    anypinentry = {
+      url = "github:tavocg/anypinentry";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    kanban = {
+      url = "github:tavocg/kanban";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     dotfiles = {
       url = "gitlab:tavocg/dotfiles";
       flake = false;
     };
 
-    codex-nix.url = "github:SecBear/codex-nix";
+    codex-nix = {
+      url = "github:SecBear/codex-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
