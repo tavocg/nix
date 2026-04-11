4271933381b8f12ccf86c90eaf4d919f09cdf467
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-10 23:39:38 -0600

    test

--- modules/features/wm/i3.nix
+++ modules/features/wm/i3.nix
@@ -35,6 +35,9 @@
   flake.nixosModules.i3 = { lib, pkgs, ... }:
     let
       x11Dir = "${inputs.dotfiles}/X11";
+      lxpolkitPackages =
+        lib.optionals (lib.hasAttrByPath [ "lxqt" "lxqt-policykit" ] pkgs) [ pkgs.lxqt.lxqt-policykit ]
+        ++ lib.optionals (pkgs ? lxpolkit) [ pkgs.lxpolkit ];
 
       mkEtcEntries = prefix: dir:
         lib.mapAttrs' (
@@ -51,57 +54,8 @@
 
         displayManager.startx = {
           enable = true;
-          generateScript = true;
-          extraCommands = ''
-            export DISPLAY="''${DISPLAY:-:0}"
-            export XAUTHORITY="''${XAUTHORITY:-$XDG_RUNTIME_DIR/Xauthority}"
-            export DESKTOP_SESSION=i3
-            export XDG_CURRENT_DESKTOP=i3
-            export XDG_SESSION_DESKTOP=i3
-            export XDG_SESSION_TYPE=x11
-
-            systemctl --user import-environment \
-              DBUS_SESSION_BUS_ADDRESS \
-              DESKTOP_SESSION \
-              DISPLAY \
-              XAUTHORITY \
-              XDG_CURRENT_DESKTOP \
-              XDG_SESSION_DESKTOP \
-              XDG_SESSION_ID \
-              XDG_SESSION_TYPE || true
-
-            dbus-update-activation-environment --systemd --all || true
-
-            # gpg-agent can stay alive across logins with stale DISPLAY/TTY data.
-            ${pkgs.gnupg}/bin/gpg-connect-agent reloadagent /bye >/dev/null 2>&1 || true
-            ${pkgs.gnupg}/bin/gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1 || true
-
-            if [ -r /etc/X11/Xresources ]; then
-              ${pkgs.xrdb}/bin/xrdb -merge /etc/X11/Xresources
-            fi
-
-            ${lib.getExe' pkgs.polkit_gnome "polkit-gnome-authentication-agent-1"} &
-            ${pkgs.xset}/bin/xset r rate 300 70
-
-            # Keep a short grace period before handing control to i3.
-            sleep 0.2
-          '';
+          # generateScript = true; # Re-enable if NixOS's generated startx/xinit integration is needed.
         };
-
-        xkb = {
-          layout = "latam,us";
-          options = "grp:win_space_toggle";
-        };
-
-        autoRepeatDelay = 300;
-        autoRepeatInterval = 14;
-
-        serverFlagsSection = ''
-          Option "StandbyTime" "30"
-          Option "SuspendTime" "0"
-          Option "OffTime" "0"
-          Option "BlankTime" "0"
-        '';
 
         windowManager.i3 = {
           enable = true;
@@ -118,17 +72,7 @@
             alacritty
             inputs.shtatus.packages.${pkgs.stdenv.hostPlatform.system}.default
             inputs.anypinentry.packages.${pkgs.stdenv.hostPlatform.system}.default
-          ];
-        };
-      };
-
-      services.libinput = {
-        enable = true;
-        mouse.accelProfile = "flat";
-        touchpad = {
-          naturalScrolling = true;
-          tapping = true;
-          disableWhileTyping = false;
+          ] ++ lxpolkitPackages;
         };
       };
 
