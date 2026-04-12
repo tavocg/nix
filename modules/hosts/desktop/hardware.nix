d954bc37c6f4edeec2b7c154da27cd2417dd54dc
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-12 01:34:55 -0600

    fmt

--- modules/hosts/desktop/hardware.nix
+++ modules/hosts/desktop/hardware.nix
@@ -1,30 +1,31 @@
 { self, inputs, ... }: {
   flake.nixosModules.desktopHardware = { config, lib, pkgs, modulesPath, ... }: {
-imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    imports = [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-amd" ];
+    boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/mapper/luks-19ff8ef6-830b-45b4-acef-0c8ac693360d";
+    fileSystems."/" = {
+      device = "/dev/mapper/luks-19ff8ef6-830b-45b4-acef-0c8ac693360d";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-19ff8ef6-830b-45b4-acef-0c8ac693360d".device = "/dev/disk/by-uuid/19ff8ef6-830b-45b4-acef-0c8ac693360d";
+    boot.initrd.luks.devices."luks-19ff8ef6-830b-45b4-acef-0c8ac693360d".device = "/dev/disk/by-uuid/19ff8ef6-830b-45b4-acef-0c8ac693360d";
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0321-DD81";
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/0321-DD81";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+    swapDevices = [ ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
 }
+
