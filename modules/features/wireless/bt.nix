c980ecc419c8e17378204f43cf3738ba311abe8f
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-12 01:29:46 -0600

    already the default

--- modules/features/wireless/bt.nix
+++ modules/features/wireless/bt.nix
@@ -13,12 +13,6 @@
           # 'false'.
           FastConnectable = true;
         };
-        Policy = {
-          # Enable all controllers when they are found. This includes
-          # adapters present on start as well as adapters that are plugged
-          # in later on. Defaults to 'true'.
-          AutoEnable = true;
-        };
       };
     };
   };
