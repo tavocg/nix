72bca4b28224fd7ab389a723a02f238ce33c7716
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-12 01:41:26 -0600

    fix

--- modules/features/system/docker.nix
+++ modules/features/system/docker.nix
@@ -1,9 +1,12 @@
 { ... }: {
   flake.nixosModules.docker = { lib, config, ... }: {
-    virtualisation.docker.enable = true;
-
-    config = lib.mkIf config.local.user.enable {
-      users.users.${config.local.user.name}.extraGroups = lib.mkAfter [ "docker" ];
-    };
+    config = lib.mkMerge [
+      {
+        virtualisation.docker.enable = true;
+      }
+      (lib.mkIf config.local.user.enable {
+        users.users.${config.local.user.name}.extraGroups = lib.mkAfter [ "docker" ];
+      })
+    ];
   };
 }
