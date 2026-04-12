e5ca4b96aa908e7ae3f5531f2faecc64c4ead9ce
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-12 15:16:08 -0600

    grrrrrrrrrrrrrrrrrrrrrrr

--- modules/features/home/pass.nix
+++ modules/features/home/pass.nix
@@ -1,5 +1,9 @@
 { ... }: {
   flake.homeModules.pass = { pkgs, config, ... }: {
+    home.sessionVariables = {
+      PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
+    };
+
     programs.password-store = {
       enable = true;
       package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
