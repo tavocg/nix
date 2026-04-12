af8cbb625ff2ef95014ec686c27ef5de6b771c92
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-12 11:58:11 -0600

    sway

--- modules/features/home/sway/foot.nix
+++
@@ -1,6 +0,0 @@
-{ inputs, ... }: {
-  flake.homeModules.swayFoot = { pkgs, ... }: {
-    home.packages = [ pkgs.foot ];
-    xdg.configFile."foot/foot.ini".source = "${inputs.dotfiles}/foot/foot.ini";
-  };
-}
