1b4f43924aa9f14d2a07912c28cf89e080483b43
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-12 21:02:21 -0600

    not needed

--- modules/features/nixos.nix
+++ modules/features/nixos.nix
@@ -1,5 +1,5 @@
-{ self, ... }: {
-  flake.nixosModules.nixos = { pkgs, ... }: {
+{ ... }: {
+  flake.nixosModules.nixos = { ... }: {
     nixpkgs.config.allowUnfree = true;
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
