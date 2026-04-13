010cdec6c80b42017346488fbae04001699b8bea
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-12 21:06:58 -0600

    packages

--- modules/features/environment/packages.nix
+++ modules/features/environment/packages.nix
@@ -3,12 +3,14 @@
     environment.systemPackages = with pkgs; [
       neovim
       yazi
+      git
       lazygit
       lazydocker
       impala
       wiremix
       bluetui
       eza
+      firefox
 
       inputs.codex-nix.packages.${pkgs.stdenv.hostPlatform.system}.default
       inputs.kanban.packages.${pkgs.stdenv.hostPlatform.system}.default
