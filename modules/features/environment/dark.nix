88488943ea2fb48e5eafdc6df63b35a738e2519c
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-12 22:28:46 -0600

    dark mode

---
+++ modules/features/environment/dark.nix
@@ -0,0 +1,16 @@
+{ ... }: {
+  flake.nixosModules.environmentDark = {
+    programs.dconf = {
+      enable = true;
+      profiles.user.databases = [
+        {
+          settings = {
+            "org/gnome/desktop/interface" = {
+              color-scheme = "prefer-dark";
+            };
+          };
+        }
+      ];
+    };
+  };
+}
--- modules/features/environment/default.nix
+++ modules/features/environment/default.nix
@@ -1,6 +1,7 @@
 { self, ... }: {
   flake.nixosModules.environment = { ... }: {
     imports = [
+      self.nixosModules.environmentDark
       self.nixosModules.environmentPackages
     ];
   };
