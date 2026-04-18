805b9195151ac3290809244686e89e18f2c29827
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-18 01:47:36 -0600

    packs

--- modules/features/i3.nix
+++ modules/features/i3.nix
@@ -1,7 +1,12 @@
 { inputs, ... }: {
   flake.nixosModules.i3 = { pkgs, ... }: {
     environment.systemPackages = [
+      pkgs.xclip
+      pkgs.bemenu
+      pkgs.scrot
+      pkgs.xdotool
       inputs.st.packages.${pkgs.stdenv.hostPlatform.system}.default
+      inputs.shtatus.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
 
     services.xserver = {
