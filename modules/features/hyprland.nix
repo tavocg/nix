230b21aedfc442c9c543c125a67429a19d9ed70b
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-19 01:35:14 -0600

    fix

--- modules/features/hyprland.nix
+++ modules/features/hyprland.nix
@@ -1,14 +1,15 @@
 { inputs, ... }: {
   flake.nixosModules.hyprland = { pkgs, ... }: {
+    local.wayland.enable = true;
+
+    environment.systemPackages = [
+      pkgs.foot
+    ];
+
     programs.hyprland = {
       enable = true;
       withUWSM = true;
       xwayland.enable = true;
-    };
-
-    xdg.portal = {
-      enable = true;
-      extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
     };
 
     environment.sessionVariables = {
@@ -16,5 +17,28 @@
       LIBVA_DRIVER_NAME = "nvidia";
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     };
+  };
+
+  flake.nixosModules.hyprlandAutologin = { config, lib, ... }: {
+    config = lib.mkMerge [
+      (lib.mkIf config.programs.hyprland.enable {
+        programs.bash.loginShellInit = ''
+          if [[ -z "$WAYLAND_DISPLAY" ]] && [[ -z "$DISPLAY" ]] && [[ "$(tty)" == /dev/tty1 ]]; then
+            exec ${
+              if config.programs.hyprland.withUWSM then
+                "${lib.getExe config.programs.uwsm.package} start -F -- /run/current-system/sw/bin/Hyprland"
+              else
+                "/run/current-system/sw/bin/Hyprland"
+            }
+          fi
+        '';
+      })
+      (lib.mkIf config.local.user.enable {
+        services.getty = {
+          autologinUser = config.local.user.name;
+          autologinOnce = false;
+        };
+      })
+    ];
   };
 }
