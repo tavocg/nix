73dd11037e2e469baf560a95c66cdf3f4eac74b5
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-21 04:36:09 -0600

    theming

--- modules/features/environment/theme.nix
+++ modules/features/environment/theme.nix
@@ -12,11 +12,50 @@
     windowingEnabled = x11Enabled || waylandEnabled;
   in {
     config = lib.mkIf windowingEnabled {
-      environment.systemPackages = [
-        pkgs.gnome-themes-extra
-      ] ++ lib.optionals x11Enabled [
-        pkgs.xsettingsd
-      ];
+      environment = {
+        systemPackages = with pkgs; [
+          gnome-themes-extra
+          adwaita-qt
+          libsForQt5.qt5ct
+          kdePackages.qt6ct
+        ];
+
+        sessionVariables = {
+          QT_QPA_PLATFORMTHEME = "qt5ct";
+        };
+      };
+
+      # I think the following will allow setting the theme without touching
+      # ~/.config/(gtk...|qt...)/
+
+      # environment.systemPackages = lib.optionals x11Enabled [
+      #   pkgs.xsettingsd
+      # ];
+
+      # programs.dconf = {
+      #   enable = true;
+      #   profiles.user.databases = [
+      #     {
+      #       settings = {
+      #         "org/gnome/desktop/interface" = {
+      #           color-scheme = "prefer-dark";
+      #           gtk-theme = "Adwaita-dark";
+      #         };
+      #       };
+      #     };
+      #   ];
+      # };
+
+      # environment.etc = {
+      #   "xdg/qt5ct/qt5ct.conf".text = ''
+      #     [Appearance]
+      #     style=adwaita-dark
+      #   '';
+      #   "xdg/qt6ct/qt6ct.conf".text = ''
+      #     [Appearance]
+      #     style=adwaita-dark
+      #   '';
+      # };
     };
   };
 }
