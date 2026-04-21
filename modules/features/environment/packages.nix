71f742aebf4d7426da0b961feaa4b253fbd043a5
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-21 04:46:54 -0600

    gui only when windowing

--- modules/features/environment/packages.nix
+++ modules/features/environment/packages.nix
@@ -4,6 +4,7 @@
       cudaEnabled = lib.attrByPath [ "local" "nvidia" "cuda" "enable" ] false config;
       x11Enabled = lib.attrByPath [ "local" "x11" "enable" ] false config;
       waylandEnabled = lib.attrByPath [ "local" "wayland" "enable" ] false config;
+      windowingEnabled = x11Enabled || waylandEnabled;
       obsPackage =
         if cudaEnabled then
           pkgs.obs-studio.override { cudaSupport = true; }
@@ -17,60 +18,64 @@
         else
           pkgs.emacs;
     in {
-      environment.systemPackages = with pkgs; [
-        neovim
-        yazi
-        git
-        trash-cli
-        tmux
-        jq
-        imv
-        vlc
-        mpv
-        eza
-        markdownlint-cli
-        ffmpeg-full
-        gnumake
-        autoconf
-        automake
-        binutils
-        m4
-        gcc
-        tesseract
-        pkg-config
-        fakeroot
-        flex
-        bison
-        tectonic
-        inputs.kanban.packages.${pkgs.stdenv.hostPlatform.system}.default
-        glow
-        pandoc
-        obsidian
-        (pass.withExtensions (exts: [exts.pass-otp]))
-        lazygit
-        lazydocker
-        impala
-        wiremix
-        bluetui
-        firefox
-        thunderbird
-        libreoffice-fresh
-        gnome-disk-utility
-        gimp
-        kdePackages.kdenlive
-        darktable
-        obsPackage
-        xournalpp
-        zathura
-        imagemagick
-        exiftool
-        emacsPackage
-        file
-        inputs.codex-nix.packages.${pkgs.stdenv.hostPlatform.system}.default
-        poppler-utils
-        bubblewrap
-        ripgrep
-        fd
-      ];
+      environment.systemPackages =
+        with pkgs;
+        [
+          neovim
+          yazi
+          git
+          trash-cli
+          tmux
+          jq
+          eza
+          markdownlint-cli
+          ffmpeg-full
+          gnumake
+          autoconf
+          automake
+          binutils
+          m4
+          gcc
+          tesseract
+          pkg-config
+          fakeroot
+          flex
+          bison
+          tectonic
+          inputs.kanban.packages.${pkgs.stdenv.hostPlatform.system}.default
+          glow
+          pandoc
+          (pass.withExtensions (exts: [ exts.pass-otp ]))
+          lazygit
+          lazydocker
+          impala
+          wiremix
+          bluetui
+          imagemagick
+          exiftool
+          emacsPackage
+          file
+          inputs.codex-nix.packages.${pkgs.stdenv.hostPlatform.system}.default
+          poppler-utils
+          bubblewrap
+          ripgrep
+          fd
+        ]
+        ++ lib.optionals windowingEnabled [
+          imv
+          vlc
+          mpv
+          obsidian
+          firefox
+          thunderbird
+          libreoffice-fresh
+          gnome-disk-utility
+          gimp
+          kdePackages.kdenlive
+          darktable
+          obsPackage
+          xournalpp
+          zathura
+        ];
     };
 }
