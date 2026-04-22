82f12014d4934a7f66f90fb89fd18fbd6fa23f93
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-22 03:11:13 -0600

    fix

--- modules/features/nixos.nix
+++ modules/features/nixos.nix
@@ -4,5 +4,8 @@
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
     nix.settings.use-xdg-base-directories = true;
+
+    # research this later?
+    programs.nix-ld.enable = true;
   };
 }
