2a5d0bc82f012239f602b31758553deb47710d72
tavocg <gustavo.calvo.cr@gmail.com>
2026-05-15 11:52:53 -0600

    gaming

--- modules/features/gaming.nix
+++ modules/features/gaming.nix
@@ -1,9 +1,19 @@
 { ... }: {
-  flake.nixosModules.gaming = { pkgs, ... }: {
-    programs.steam.enable = true;
+  flake.nixosModules.gaming = { config, lib, ... }: {
+    config = lib.mkMerge [
+      {
+        hardware.uinput.enable = true;
 
-    environment.systemPackages = with pkgs; [
-      lutris
+        programs.steam.enable = true;
+
+        services.sunshine = {
+          enable = true;
+          openFirewall = true;
+        };
+      }
+      (lib.mkIf config.local.user.enable {
+        users.users.${config.local.user.name}.extraGroups = lib.mkAfter [ "uinput" ];
+      })
     ];
   };
 }
