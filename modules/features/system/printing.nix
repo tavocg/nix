fe83ec51fb2fcd11d0a2dcd278e76f8aaa39fdc8
tavocg <gustavo.calvo.cr@gmail.com>
2026-05-31 16:06:39 -0600

    rm

--- modules/features/system/printing.nix
+++ modules/features/system/printing.nix
@@ -22,7 +22,6 @@
           description = "Epson L3150 Series";
           location = "Home";
 
-          #deviceUri = "socket://192.168.100.91:9100";
           deviceUri = "dnssd://EPSON%20L3150%20Series._pdl-datastream._tcp.local/";
 
           model = "epson-inkjet-printer-escpr/Epson-L3150_Series-epson-escpr-en.ppd";
