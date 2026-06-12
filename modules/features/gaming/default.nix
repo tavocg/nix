cddc099bc0910638791579117d44f31de27b0eae
tavocg <gustavo.calvo.cr@gmail.com>
2026-06-11 23:34:46 -0600

    lutris

--- modules/features/gaming/default.nix
+++ modules/features/gaming/default.nix
@@ -1,10 +1,14 @@
 { self, ... }: {
-  flake.nixosModules.gaming = { ... }: {
+  flake.nixosModules.gaming = { pkgs, ... }: {
     imports = [
       self.nixosModules.gamingMinecraft
       self.nixosModules.gamingSunshine
       self.nixosModules.gamingSteam
       self.nixosModules.gamingFlightgear
+    ];
+
+    environment.systemPackages = with pkgs; [
+      lutris
     ];
   };
 }
