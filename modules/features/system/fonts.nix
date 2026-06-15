82ce39c1772654214119c5d7086f83312b866667
tavocg <gustavo.calvo.cr@gmail.com>
2026-06-15 13:12:01 -0600

    fonts

--- modules/features/system/fonts.nix
+++ modules/features/system/fonts.nix
@@ -1,9 +1,17 @@
 { ... }: {
   flake.nixosModules.fonts = { pkgs, ... }: {
     fonts.packages = with pkgs; [
+      cm_unicode
       corefonts
+      iosevka
       jetbrains-mono
+      lmodern
+      newcomputermodern
+      nerd-fonts.iosevka
+      nerd-fonts.iosevka-term
       nerd-fonts.jetbrains-mono
+      nerd-fonts.symbols-only
+      symbola
     ];
   };
 }
