5a154bd0ef8426e8b4b8e4377617630583e17e56
tavocg <gustavo.calvo.cr@gmail.com>
2026-06-16 16:45:32 -0600

    ato

--- modules/features/system/fonts.nix
+++ modules/features/system/fonts.nix
@@ -1,12 +1,14 @@
 { ... }: {
   flake.nixosModules.fonts = { pkgs, ... }:
   let
+    iosevkaAile = pkgs.iosevka-bin.override { variant = "Aile"; };
     iosevkaEtoile = pkgs.iosevka-bin.override { variant = "Etoile"; };
   in {
     fonts.packages = with pkgs; [
       cm_unicode
       corefonts
       iosevka
+      iosevkaAile
       iosevkaEtoile
       jetbrains-mono
       lmodern
