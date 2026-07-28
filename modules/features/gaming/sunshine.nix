8830926acd96d76cf3c6864ecc7fdaa471c2617e
tavocg <gustavo.calvo.cr@gmail.com>
2026-07-28 00:04:22 -0600

    sunshine

--- modules/features/gaming/sunshine.nix
+++ modules/features/gaming/sunshine.nix
@@ -1,17 +1,30 @@
 { ... }: {
-  flake.nixosModules.gamingSunshine = { config, lib, ... }: {
-    config = lib.mkMerge [
-      {
-        hardware.uinput.enable = true;
+  flake.nixosModules.gamingSunshine = { config, lib, pkgs, ... }:
+    let
+      cudaEnabled = lib.attrByPath [ "local" "nvidia" "cuda" "enable" ] false config;
+      sunshinePackage =
+        if cudaEnabled then
+          pkgs.sunshine.override {
+            cudaSupport = true;
+            cudaPackages = pkgs.cudaPackages;
+          }
+        else
+          pkgs.sunshine;
+    in
+    {
+      config = lib.mkMerge [
+        {
+          hardware.uinput.enable = true;
 
-        services.sunshine = {
-          enable = true;
-          openFirewall = true;
-        };
-      }
-      (lib.mkIf config.local.user.enable {
-        users.users.${config.local.user.name}.extraGroups = lib.mkAfter [ "uinput" ];
-      })
-    ];
-  };
+          services.sunshine = {
+            enable = true;
+            openFirewall = true;
+            package = sunshinePackage;
+          };
+        }
+        (lib.mkIf config.local.user.enable {
+          users.users.${config.local.user.name}.extraGroups = lib.mkAfter [ "uinput" ];
+        })
+      ];
+    };
 }
