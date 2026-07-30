da11a34e4b73f998e44ecd55c92229e26ec4668e
tavocg <gustavo.calvo.cr@gmail.com>
2026-07-30 00:35:51 -0600

    fix

--- modules/features/gaming/steam.nix
+++ modules/features/gaming/steam.nix
@@ -3,7 +3,7 @@
     programs = {
       gamescope = {
         enable = true;
-        capSysNice = true;
+        capSysNice = false; # should be true but bwrap complains
       };
 
       steam = {
