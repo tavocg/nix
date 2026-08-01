d72a31ac4a318d9b6dc1bbfa52910386fcbdfb66
tavocg <gustavo.calvo.cr@gmail.com>
2026-07-31 22:43:52 -0600

    test

--- modules/features/gaming/sunshine.nix
+++ modules/features/gaming/sunshine.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ inputs, ... }: {
   flake.nixosModules.gamingSunshine =
     {
       config,
@@ -8,6 +8,13 @@
     }:
     let
       cudaEnabled = lib.attrByPath [ "local" "nvidia" "cuda" "enable" ] false config;
+      sunshineDotfilesDir = inputs.dotfiles + "/sunshine";
+      sunshineConfigFile = pkgs.writeText "sunshine.conf" (
+        builtins.readFile (sunshineDotfilesDir + "/sunshine.conf")
+      );
+      sunshineAppsFile = pkgs.writeText "apps.json" (
+        builtins.readFile (sunshineDotfilesDir + "/apps.json")
+      );
       sunshinePackage =
         if cudaEnabled then
           pkgs.sunshine.override {
@@ -26,11 +33,17 @@
             enable = true;
             openFirewall = true;
             package = sunshinePackage;
-            autoStart = false;
+            autoStart = true;
           };
         }
         (lib.mkIf config.local.user.enable {
           users.users.${config.local.user.name}.extraGroups = lib.mkAfter [ "uinput" ];
+
+          systemd.tmpfiles.rules = [
+            "d /home/${config.local.user.name}/.config/sunshine 0755 ${config.local.user.name} users - -"
+            "L+ /home/${config.local.user.name}/.config/sunshine/sunshine.conf - - - - ${sunshineConfigFile}"
+            "L+ /home/${config.local.user.name}/.config/sunshine/apps.json - - - - ${sunshineAppsFile}"
+          ];
         })
       ];
     };
