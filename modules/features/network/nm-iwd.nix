7128a9b8436acf44658dd889a765183179890251
tavocg <gustavo.calvo.cr@gmail.com>
2026-08-27 02:23:43 -0600

    fix

--- modules/features/network/nm-iwd.nix
+++ modules/features/network/nm-iwd.nix
@@ -1,8 +1,9 @@
 { ... }: {
-  flake.nixosModules.networkManagerIWD = { ... }: {
+  flake.nixosModules.networkManagerIWD = { config, lib, ... }: {
     networking.dhcpcd.enable = false;
     networking.networkmanager.enable = true;
     networking.networkmanager.wifi.backend = "iwd";
+    users.users.${config.local.user.name}.extraGroups = lib.mkAfter [ "networkmanager" ];
     services.resolved.enable = true;
 
     networking.wireless.iwd = {
